2b5189ad2d6abab3daf5209f78fb0f82d641621a
1deterministic <22532656+1deterministic@users.noreply.github.com>
2020-02-09 11:56:17 -0300

    Added MySQL Workbench for Debian

--- apps/mysql-workbench.sh
+++ apps/mysql-workbench.sh
@@ -2,13 +2,17 @@
 source config.sh
 
 link_mysql_workbench_rpm="https://cdn.mysql.com//Downloads/MySQLGUITools/mysql-workbench-community-8.0.18-1.fc31.x86_64.rpm"
+link_mysql_workbench_deb="https://dev.mysql.com/get/Downloads/MySQLGUITools/mysql-workbench-community_8.0.19-1ubuntu18.04_amd64.deb"
 
 if [[ $osname == $archlinux ]]; then
     sudo pacman -S mysql-workbench --noconfirm
 fi
 
 if [[ $osname == $debian ]]; then
-    $missing
+    mkdir /tmp/mysql-workbench
+    wget $link_mysql_workbench_deb -O /tmp/mysql-workbench/mysql-workbench.deb
+    sudo apt install /tmp/mysql-workbench/mysql-workbench.deb -y
+    rm -r /tmp/mysql-workbench
 fi
 
 if [[ $osname == $fedora ]]; then
