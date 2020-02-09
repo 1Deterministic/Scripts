921213d728438e946b9356350f7c2b00f8610493
1deterministic <22532656+1deterministic@users.noreply.github.com>
2020-02-09 14:40:30 -0300

    Changed android-studio for Arch to use the same scripts as the other distros

--- apps/android-studio.sh
+++ apps/android-studio.sh
@@ -4,13 +4,11 @@ source config.sh
 link_android_studio="https://dl.google.com/dl/android/studio/ide-zips/3.5.1.0/android-studio-ide-191.5900203-linux.tar.gz"
 
 if [[ $osname == $archlinux ]]; then
-    sudo mount -o remount,size=16G /tmp
-    echo "The machine can freeze, be patient"
-    git clone https://aur.archlinux.org/android-studio
-    cd android-studio
-    makepkg -si --noconfirm
-    cd ..
-    rm -rf android-studio
+    sudo mkdir /tmp/android-studio
+    sudo wget $link_android_studio -O /tmp/android-studio/android-studio.tar.gz
+    sudo tar xvzf /tmp/android-studio/android-studio.tar.gz --directory /opt
+    sudo rm -r /tmp/android-studio
+    sudo bash -c "cat strings/launcher-android-studio > /usr/share/applications/android-studio.desktop"
     sudo mkdir /opt/android-sdk
     sudo chown $username -R /opt/android-sdk
 fi
