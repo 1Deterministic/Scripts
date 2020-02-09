31ab1099468440072f715c16f1332707e7138d7a
1deterministic <22532656+1deterministic@users.noreply.github.com>
2020-02-09 14:48:30 -0300

    Changed virtualbox on Arch to use the same scripts as the other distros for the extension pack, also added an updated link for Arch and Fedora

--- apps/virtualbox.sh
+++ apps/virtualbox.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 source config.sh
 
-link_virtualbox_ext="https://download.virtualbox.org/virtualbox/5.2.16/Oracle_VM_VirtualBox_Extension_Pack-5.2.16.vbox-extpack"
+link_virtualbox_ext52="https://download.virtualbox.org/virtualbox/5.2.16/Oracle_VM_VirtualBox_Extension_Pack-5.2.16.vbox-extpack"
+link_virtualbox_ext61="https://download.virtualbox.org/virtualbox/6.1.2/Oracle_VM_VirtualBox_Extension_Pack-6.1.2.vbox-extpack"
 
 if [[ $osname == $archlinux ]]; then
     sudo pacman -S virtualbox-host-modules-arch --noconfirm
     sudo pacman -S virtualbox --noconfirm
-    git clone https://aur.archlinux.org/virtualbox-ext-oracle
-    cd virtualbox-ext-oracle
-    makepkg -si --noconfirm
-    cd ..
-    rm -rf virtualbox-ext-oracle
     sudo usermod -aG vboxusers $username
+    sudo mkdir /tmp/virtualbox
+    sudo wget $link_virtualbox_ext61 -O /tmp/virtualbox/virtualbox_ext.vbox-extpack
+    sudo VBoxManage extpack install --replace /tmp/virtualbox/virtualbox_ext.vbox-extpack
+    sudo rm -r /tmp/virtualbox
 fi
 
 if [[ $osname == $debian ]]; then
@@ -21,7 +21,7 @@ if [[ $osname == $debian ]]; then
     sudo apt install virtualbox -y
     sudo gpasswd -a $username vboxusers
     sudo mkdir /tmp/virtualbox
-    sudo wget $link_virtualbox_ext -O /tmp/virtualbox/virtualbox_ext.vbox-extpack
+    sudo wget $link_virtualbox_ext52 -O /tmp/virtualbox/virtualbox_ext.vbox-extpack
     sudo VBoxManage extpack install --replace /tmp/virtualbox/virtualbox_ext.vbox-extpack
     sudo rm -r /tmp/virtualbox
 fi
@@ -34,7 +34,7 @@ if [[ $osname == $fedora ]]; then
     sudo adduser $username -g vboxusers
     sudo usermod -a -G vboxusers $username
     sudo mkdir /tmp/virtualbox
-    sudo wget $link_virtualbox_ext -O /tmp/virtualbox/virtualbox_ext.vbox-extpack
+    sudo wget $link_virtualbox_ext61 -O /tmp/virtualbox/virtualbox_ext.vbox-extpack
     sudo VBoxManage extpack install --replace /tmp/virtualbox/virtualbox_ext.vbox-extpack
     sudo rm -r /tmp/virtualbox
 fi
@@ -44,6 +44,6 @@ if [[ $osname == $ubuntu ]]; then
     sudo apt install build-essential -y
     sudo apt install linux-headers-$(uname -r) -y
     sudo apt install virtualbox -y
-    sudo gpasswd -a $username vboxusers
+    sudo gpasswd -a $link_virtualbox_ext52 vboxusers
     sudo apt install virtualbox-ext-pack -y
 fi
