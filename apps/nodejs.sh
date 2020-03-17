1475ac7e20731d3c8782e2362001702de7a95b40
1deterministic <22532656+1deterministic@users.noreply.github.com>
2020-03-17 15:27:01 -0300

    Well, this guy may have a point https://invidio.us/watch?v=24tQRwIRP_w (16:25)

--- apps/nodejs.sh
+++ apps/nodejs.sh
@@ -7,10 +7,12 @@ fi
 
 if [[ $osname == $debian ]]; then
     sudo apt install nodejs npm -y
+    npm config set ignore-scripts true
 fi
 
 if [[ $osname == $fedora ]]; then
     sudo dnf install nodejs -y
+    npm config set ignore-scripts true
 fi
 
 if [[ $osname == $ubuntu ]]; then
